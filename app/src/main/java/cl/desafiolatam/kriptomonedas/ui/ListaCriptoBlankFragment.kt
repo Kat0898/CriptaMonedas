b13d28eed2ff4bcb5c6b97fa30ac00444ab7b40a
Katy <katerin.ilustracion.dv@gmail.com>
2022-04-25 22:06:40 -0400

    commit SheredPre

--- app/src/main/java/cl/desafiolatam/kriptomonedas/ui/ListaCriptoBlankFragment.kt
+++ app/src/main/java/cl/desafiolatam/kriptomonedas/ui/ListaCriptoBlankFragment.kt
@@ -1,11 +1,14 @@
 package cl.desafiolatam.kriptomonedas.ui
 
+import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
@@ -29,11 +32,22 @@ class ListaCriptoBlankFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentListaCriptoBlankBinding.inflate(layoutInflater)
+        sh = requireActivity().applicationContext.getSharedPreferences(getString(R.string.userd),MODE_PRIVATE)
+
 
         val layoutManager = LinearLayoutManager(requireContext())
 
         with(binding)
         {
+           btnIngresarList.setOnClickListener {
+               val user = edtUsuarioList.text.toString()
+               val commit = sh.edit().putString(getString(R.string.bienvenido), user).commit()
+
+               Toast.makeText(requireContext(), getString(R.string.registro), Toast.LENGTH_LONG).show()
+           }
+
+
+
             rvkRIPTO.adapter = adapter
             rvkRIPTO.layoutManager = layoutManager
 
@@ -53,6 +67,10 @@ class ListaCriptoBlankFragment : Fragment() {
         })
 
         return binding.root
+
+
+
     }
+
 
 }
